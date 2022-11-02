63f33620e3fe2751fd764a0b399da9aaff18a525
Ludo LM <ludolm@hotmail.fr>
2022-11-02 23:35:48 +0100

    ajout de sortie/creation

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/{id}', name: 'sortie_detail', requirements: ['id' => '\d+'])]
+    #[Route('/sortie/{id}', name: 'sortie_detail', requirements: ['id' => '\d+'])]
     public function index(SortieRepository $sortieRepository, int $id): Response
     {
 
@@ -17,6 +21,24 @@ class SortieController extends AbstractController
 
         return $this->render('sortie/detail.html.twig', [
             'sortie' => $sortie,
+        ]);
+    }
+    #[Route('/sortie/creation', name:"sortie!creation")]
+    public function new(Request $request, EntityManagerInterface $em)
+    {
+        $sortie = new Sortie();
+        $serieForm  = $this->createForm(SortieType::class, $sortie);
+
+        $serieForm->handleRequest($request);
+
+        if($serieForm->isSubmitted() && $serieForm->isValid()){
+            $em->persist($sortie);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render("sortie/creationSortie.html.twig", [
+            "sortieForm" => $serieForm->createView()
         ]);
     }
 }
